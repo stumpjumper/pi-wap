abf9b9848e09f28f421345799a76ffd60b1097ec
Alfred Lorber <Alfred@TheLorbers.com>
2019-09-14 15:02:39 -0600

    "flask_hello.py: Prints wireless access point status as default using list_wifi_clients.py"

--- flask/flask_hello.py
+++ flask/flask_hello.py
@@ -1,14 +1,42 @@
 #!/usr/bin/env python
 
 from flask import Flask
+from list_wifi_clients import *
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello-world')
 def hello_world():
-    msg="""
-    This is a line of text<br>
-    And another line of text<br>
-    And one more!<br>
+  msg="""
+  Hello world!<br>
+  <br>
+  This is a line of text<br>
+  And another line of text<br>
+  And one more!<br>
 """
-    return msg
-    
+  return msg
+
+
+@app.route('/')
+def dhpc_status():
+
+  myListWiFiClients = ListWiFiClients()
+
+  #print ("Connected Devices:")
+  conDevicesString = myListWiFiClients.getConDevicesTableString()
+  #conDevicesString = conDevicesString.replace('\n', '<br>\n')
+
+  #print ("DHCP Leases:")
+  dhcpLeasesString = myListWiFiClients.getDHCPLeasesTableString()
+  #dhcpLeasesString = dhcpLeasesString.replace('\n', '<br>\n')
+
+  msg ="Connected Devices\n"
+  msg+="<pre>\n"
+  msg+=conDevicesString
+  msg+="</pre>\n"
+  msg+="<br>DHCP Leases\n"
+  msg+="<pre>\n"
+  msg+=dhcpLeasesString
+  msg+="</pre>\n"
+  return msg
+
